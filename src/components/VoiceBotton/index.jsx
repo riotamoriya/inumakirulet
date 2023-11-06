195cf66f95d6740cd42b2ce8bc8ac948acd1b999
riotamoriya <riotamoriya@gmail.com>
2023-11-06 20:57:06 +0900

    css化

--- src/components/VoiceBotton/index.jsx
+++ src/components/VoiceBotton/index.jsx
@@ -1,32 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
-
-
-// ボタンのスタイル
-const buttonStyle = {
-  padding: '1rem 2rem',
-  fontSize: '1rem',
-  fontWeight: 'bold',
-  color: '#FFF',
-  background: 'linear-gradient(45deg, #fe6b8b 30%, #ff8e53 90%)',
-  border: 'none',
-  borderRadius: '50px',
-  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-  cursor: 'pointer',
-  outline: 'none',
-  transition: 'background-color 0.3s ease-in-out, transform 0.2s ease',
-};
-
-// ホバースタイル
-const buttonHoverStyle = {
-  backgroundColor: '#ff8e53', // ホバー時の背景色
-  transform: 'scale(1.05)',
-  boxShadow: '0 4px 20px 0 rgba(255, 105, 135, .5)',
-};
-
+import './style.css';
 
 const CharacterImage = () => {
-
   const data = useStaticQuery(graphql`
   query fetchSounds{
     normal: file(relativePath: { eq: "shake.mp3" }) {
@@ -37,15 +13,6 @@ const CharacterImage = () => {
     }
   }
 `);
-
-  // ホバー状態の管理
-  const [isHovered, setIsHovered] = useState(false);
-  // ボタンにホバー状態を適用する関数
-  const applyHoverStyle = (hoverState) => {
-    setIsHovered(hoverState);
-  };
-
-  // クエリ部分は変わらず...
   const [audioContext, setAudioContext] = useState(null);
 
   // Web Audio API用のオーディオデータ
@@ -81,16 +48,12 @@ const CharacterImage = () => {
   };
 
   return (
-    <>
-      <button
-        onClick={playSound}
-        onMouseEnter={() => applyHoverStyle(true)}
-        onMouseLeave={() => applyHoverStyle(false)}
-        style={isHovered ? {...buttonStyle, ...buttonHoverStyle} : buttonStyle}
-      >
-        {/* Play Sound */}
-      </button>
-    </>
+    <button
+      onClick={playSound}
+      className="buttonStyle"
+    >
+      Say
+    </button>
   );
 };
 
