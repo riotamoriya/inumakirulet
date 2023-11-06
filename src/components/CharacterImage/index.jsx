2e8c3d881cb2b67fdbde940c2ad8bd6c224d8f74
riotamoriya <riotamoriya@gmail.com>
2023-11-06 21:40:33 +0900

    cssへのまとめとSEO対策

--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import './style.css';
 
 const CharacterImage = () => {
   const data = useStaticQuery(graphql`
@@ -20,18 +21,10 @@ const CharacterImage = () => {
   `);
   const image = getImage(data.file.childImageSharp.gatsbyImageData);
   
-  // スタイルオブジェクトを定義
-  const imageStyle = {
-    width: '50vh',
-    height: '50vh',
-    // 画像を親要素に収まるように設定
-    objectFit: 'cover',
-    objectPosition: 'center',
-  };
-
   return (
-    <div style={{ marginBottom: '5vh' }}> {/* 画像とボタンの間隔を調整 */}
-      {image && <GatsbyImage image={image} alt="狗巻棘" style={imageStyle}/>}
+    <div className="character-image-container"> {/* 画像とボタンの間隔を調整 */}
+      {image && 
+        <GatsbyImage image={image} alt="狗巻棘" className="character-image"/>}
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,15 @@
 import * as React from "react";
-
-import Layouts from "../components/layouts";
 import CharacterImage from "../components/CharacterImage";
 import VoiceBotton from '../components/VoiceBotton';
 
 const IndexPage = () => {
   return (
-    <Layouts>
-      <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', padding: '5vh 0' }}>
-        {/* 画像を中央に配置 */}
-        <CharacterImage />
-        {/* ボタンを中央に配置 */}
-        <VoiceBotton />
-      </main>
-    </Layouts>
+    <main style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', padding: '5vh 0' }}>
+      {/* 画像を中央に配置 */}
+      <CharacterImage />
+      {/* ボタンを中央に配置 */}
+      <VoiceBotton />
+    </main>
   );
 };
 
@@ -27,13 +23,13 @@ export const Head = () => (
     {/* Open Graph / Facebook */}
     <meta property="og:title" content="狗巻棘確率機" />
     <meta property="og:description" content="狗巻棘確率機" />
-    <meta property="og:image" content="/inumaki.ico" />
+    <meta property="og:image" content="https://riotamoriya.github.io/inumakirulet/inumaki.jpg" />
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://riotamoriya.github.io/tokidokionigiri/" />
+    <meta property="og:url" content="https://riotamoriya.github.io/inumakirulet/" />
     {/* Twitter */}
     <meta name="twitter:card" content="狗巻棘確率機" />
     <meta name="twitter:title" content="狗巻棘確率機" />
     <meta name="twitter:description" content="狗巻棘確率機" />
-    <meta name="twitter:image" content="/inumaki.ico" />
+    <meta name="twitter:image" content="https://riotamoriya.github.io/inumakirulet/inumaki.jpg" />
   </>
 );
